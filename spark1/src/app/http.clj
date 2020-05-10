c1ee3c7beb7473e650cd38e8f73cc6de8f24e24b
seeris <sergei.udris@gmail.com>
2020-05-10 08:56:01 +0300

    build

--- spark1/src/app/http.clj
+++ spark1/src/app/http.clj
@@ -2,8 +2,7 @@
   (:require
    [clojure.core.async :as a :refer [<! >! <!! timeout chan alt! go close!
                                      >!! <!! alt!! alts! alts!! take! put! mult tap untap
-                                     thread pub sub sliding-buffer mix admix unmix]]
-   [app.api])
+                                     thread pub sub sliding-buffer mix admix unmix]])
   (:import
    spark.Spark
    spark.Route))
