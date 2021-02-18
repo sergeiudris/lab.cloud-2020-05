0419a00374f850742884dd0779397d0553a7baa4
Sergei Udris <sergei.udris@gmail.com>
2021-02-18 16:20:35 +0300

    spark1: GraalVM native image works with core.async

--- spark1/src/app/main.clj
+++ spark1/src/app/main.clj
@@ -6,8 +6,25 @@
    [app.http])
   (:gen-class))
 
+(def counter1 (atom 0))
+(def counter2 (atom 0))
+
+(def foo| (chan (sliding-buffer 10)))
 
 (defn -main [& args]
   #_(println "hello")
   #_(Thread/sleep Long/MAX_VALUE)
+  (go (loop []
+        (<! (timeout 1000))
+        (swap! counter1 inc)
+        (println ::loop-a @counter1)
+        (recur)))
+  (go (loop []
+        (<! (timeout 1000))
+        (>! foo| @counter1)
+        (recur)))
+  (go (loop []
+        (when-let [value (<! foo|)]
+          (println ::loop-b value)
+          (recur))))
   (app.http/start))
